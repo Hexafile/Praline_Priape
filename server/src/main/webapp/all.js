cdd333879f81bd7bf1f82122c1d53aee4f116407
Jeremy STEENKISTE <jeremy.steenkiste@etudiant.univ-lille1.fr>
2017-03-23 09:37:50 +0100

    Probleme chargement de la page

--- server/src/main/webapp/all.js
+++ server/src/main/webapp/all.js
@@ -1,72 +1,71 @@
 function getUserBdd(name) {
-	getUserGeneric(name, "v1/user/");
+    getUserGeneric(name, "v1/user/");
 }
 
 function getUserGeneric(name, url) {
-	$.getJSON(url + name, function(data) {
-		afficheUser(data);
-	});
+    $.getJSON(url + name, function (data) {
+        afficheUser(data);
+    });
 }
 
 function getForAll() {
-	getSecure("v1/secure/who");
+    getSecure("v1/secure/who");
 }
 
 function getByAnnotation() {
-	getSecure("v1/secure/byannotation");
+    getSecure("v1/secure/byannotation");
 }
 
- function getSecure(url) {
- if($("#userlogin").val() != "") {
-     $.ajax
-     ({
-       type: "GET",
-       url: url,
-       dataType: 'json',
-       beforeSend : function(req) {
-        req.setRequestHeader("Authorization", "Basic " + btoa($("#userlogin").val() + ":" + $("#passwdlogin").val()));
-       },
-       success: function (data) {
-        afficheUser(data);
-        $("#ok").show();   
-       },
-       error : function(jqXHR, textStatus, errorThrown) {
-       			alert('error: ' + textStatus);
+function getSecure(url) {
+    if ($("#userlogin").val() != "") {
+        $.ajax({
+            type: "GET",
+            url: url,
+            dataType: 'json',
+            beforeSend: function (req) {
+                req.setRequestHeader("Authorization", "Basic " + btoa($("#userlogin").val() + ":" + $("#passwdlogin").val()));
+            },
+            success: function (data) {
+                afficheUser(data);
+                $("#ok").show();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                alert('error: ' + textStatus);
                 $("#pasok").show();
-       		}
-     });
-     } else {
-     $.getJSON(url, function(data) {
-     	    afficheUser(data);
+            }
         });
-     }
- }
+    } else {
+        $.getJSON(url, function (data) {
+            afficheUser(data);
+        });
+    }
+}
 
 function postUserBdd(name, alias, email, pwd) {
     postUserGeneric(name, alias, email, pwd, "v1/user/");
 }
 
 function postUserGeneric(name, alias, email, pwd, url) {
-	console.log("postUserGeneric " + url)
-	$.ajax({
-		type : 'POST',
-		contentType : 'application/json',
-		url : url,
-		dataType : "json",
-		data : JSON.stringify({
-			"name" : name,
-			"alias" : alias,
-			"email" : email,
-			"password" : pwd,
-			"id" : 0
-		}),
-		success : function(data, textStatus, jqXHR) {
-			afficheUser(data);
-		},
-		error : function(jqXHR, textStatus, errorThrown) {
-			console.log('postUser error: ' + textStatus);
-		}
-	});
+    console.log("postUserGeneric " + url)
+    $.ajax({
+        type: 'POST',
+        contentType: 'application/json',
+        url: url,
+        dataType: "json",
+        data: JSON.stringify({
+            "name": name,
+            "alias": alias,
+            "email": email,
+            "password": pwd,
+            "id": 0
+        }),
+        success: function (data, textStatus, jqXHR) {
+            afficheUser(data);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log('postUser error: ' + textStatus);
+        }
+    });
 }
 
 function listUsersBdd() {
@@ -74,46 +73,46 @@ function listUsersBdd() {
 }
 
 function listUsersGeneric(url) {
-	$.getJSON(url, function(data) {
-		afficheListUsers(data)
-	});
+    $.getJSON(url, function (data) {
+        afficheListUsers(data)
+    });
 }
 
 function afficheUser(data) {
-	console.log(data);
-	$("#reponse").html(data.id + " : <b>" + data.alias + "</b> (" + data.name + ")");
+    console.log(data);
+    $("#reponse").html(data.id + " : <b>" + data.alias + "</b> (" + data.name + ")");
 }
 
 function afficheListUsers(data) {
-	var html = '<ul>';
-	var index = 0;
-	for (index = 0; index < data.length; ++index) {
-		html = html + "<li>"+ data[index].name + "</li>";
-	}
-	html = html + "</ul>";
-	$("#reponse").html(html);
+    var html = '<ul>';
+    var index = 0;
+    for (index = 0; index < data.length; ++index) {
+        html = html + "<li>" + data[index].name + "</li>";
+    }
+    html = html + "</ul>";
+    $("#reponse").html(html);
 }
 
-function loadPage(){
+function loadPage() {
     $("#inscriptionContainer").hide();
 }
 
-function signUp(){
+function signUp() {
     $("#mainContainer").hide();
     $("#inscriptionContainer").show();
 }
 
-function signInForm(){
+function signIn() {
     $("#mainContainer").hide();
 }
 
 /* Fonction d'inscription */
-$(document).ready(function() {
-    $("#post-bdd").click(function() {
-    postUserBdd(
-                    $('#userdb').val(),
-                    $('#aliasdb').val(),
-                    $('#emaildb').val(),
-                    $('#passwddb').val())
-            });
-        });
+$(document).ready(function () {
+    $("#post-bdd").click(function () {
+        postUserBdd(
+            $('#userdb').val(),
+            $('#aliasdb').val(),
+            $('#emaildb').val(),
+            $('#passwddb').val())
+    });
+});

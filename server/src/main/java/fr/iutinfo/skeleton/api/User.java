776a9998880d8f5f23aefcde7c90f8d8a8b6a030
Hexafile <thomas.plouchart@etudiant.univ-lille1.fr>
2017-03-23 16:12:35 +0100

    test

--- server/src/main/java/fr/iutinfo/skeleton/api/User.java
+++ server/src/main/java/fr/iutinfo/skeleton/api/User.java
@@ -11,47 +11,47 @@ import java.security.Principal;
 import java.security.SecureRandom;
 
 public class User implements Principal {
-    final static Logger logger = LoggerFactory.getLogger(User.class);
-    private static User anonymous = new User(-1, "Anonymous", "anonym");
-    
+	final static Logger logger = LoggerFactory.getLogger(User.class);
+	private static User anonymous = new User(-1, "Anonymous", "anonym");
+
 	private int id = 0;
-    private int role=0;
-    private String name;
-    private String surname;
-    private String alias;
-    private String adresse;
-    private String societe;
-    private int tel;
-    private int sexe;
-    private int ptsFidelite;
-    private java.util.Date dateNaissance = new java.util.Date();
-    private java.util.Date dateInscription = new java.util.Date();
-    private boolean newsLetter;
-    private String email;
-    private String password;
-    private String passwdHash;
-    private String salt;
-    private String search;
+	private int role = 0;
+	private String name;
+	private String surname;
+	private String alias;
+	private String adresse;
+	private String societe;
+	private int tel;
+	private int sexe;
+	private int ptsFidelite;
+	private java.util.Date dateNaissance = new java.util.Date();
+	private java.util.Date dateInscription = new java.util.Date();
+	private boolean newsLetter;
+	private String email;
+	private String password;
+	private String passwdHash;
+	private String salt;
+	private String search;
 
-    public User(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+	public User(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
 
-    public User(int id, String name, String alias) {
-        this.id = id;
-        this.name = name;
-        this.alias = alias;
-    }
+	public User(int id, String name, String alias) {
+		this.id = id;
+		this.name = name;
+		this.alias = alias;
+	}
 
-    public User() {
-    }
+	public User() {
+	}
 
-    public static User getAnonymousUser() {
-        return anonymous;
-    }
+	public static User getAnonymousUser() {
+		return anonymous;
+	}
 
-    public String getSociete() {
+	public String getSociete() {
 		return societe;
 	}
 
@@ -60,10 +60,10 @@ public class User implements Principal {
 	}
 
 	public String getEmail() {
-        return email;
-    }
+		return email;
+	}
 
-    public int getRole() {
+	public int getRole() {
 		return role;
 	}
 
@@ -120,26 +120,26 @@ public class User implements Principal {
 	}
 
 	public void setEmail(String email) {
-        this.email = email;
-    }
+		this.email = email;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
-    
-    public String getSurname() {
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
 		return surname;
 	}
 
@@ -147,7 +147,7 @@ public class User implements Principal {
 		this.surname = surname;
 	}
 
-    public java.util.Date getDateInscription() {
+	public java.util.Date getDateInscription() {
 		return dateInscription;
 	}
 
@@ -156,133 +156,138 @@ public class User implements Principal {
 	}
 
 	public String getPassword() {
-        return this.password;
-    }
+		return this.password;
+	}
 
-    public void setPassword(String password) {
-        passwdHash = buildHash(password, getSalt());
-        this.password = password;
-    }
+	public void setPassword(String password) {
+		passwdHash = buildHash(password, getSalt());
+		this.password = password;
+	}
 
-    private String buildHash(String password, String s) {
-        Hasher hasher = Hashing.sha256().newHasher();
-        hasher.putString(password + s, Charsets.UTF_8);
-        return hasher.hash().toString();
-    }
+	private String buildHash(String password, String s) {
+		Hasher hasher = Hashing.sha256().newHasher();
+		hasher.putString(password + s, Charsets.UTF_8);
+		return hasher.hash().toString();
+	}
 
-    public boolean isGoodPassword(String password) {
-        if (isAnonymous()) {
-            return false;
-        }
-        String hash = buildHash(password, getSalt());
-        return hash.equals(getPasswdHash());
-    }
+	public boolean isGoodPassword(String password) {
+		if (isAnonymous()) {
+			return false;
+		}
+		String hash = buildHash(password, getSalt());
+		return hash.equals(getPasswdHash());
+	}
 
-    public String getPasswdHash() {
-        return passwdHash;
-    }
+	public String getPasswdHash() {
+		return passwdHash;
+	}
 
-    public void setPasswdHash(String passwdHash) {
-        this.passwdHash = passwdHash;
-    }
+	public void setPasswdHash(String passwdHash) {
+		this.passwdHash = passwdHash;
+	}
 
-    @Override
-    public boolean equals(Object arg) {
-        if (getClass() != arg.getClass())
-            return false;
-        User user = (User) arg;
-        return name.equals(user.name) && alias.equals(user.alias) && email.equals(user.email) && passwdHash.equals(user.getPasswdHash()) && salt.equals((user.getSalt()));
-    }
+	@Override
+	public boolean equals(Object arg) {
+		if (getClass() != arg.getClass())
+			return false;
+		User user = (User) arg;
+		return name.equals(user.name) && alias.equals(user.alias) && email.equals(user.email)
+				&& passwdHash.equals(user.getPasswdHash()) && salt.equals((user.getSalt()));
+	}
 
-    @Override
-    public String toString() {
-        return id + ": " + alias + ", " + name + ", " + surname + " <" + email + ">";
-    }
+	@Override
+	public String toString() {
+		return id + ": " + alias + ", " + name + ", " + surname + " <" + email + ">";
+	}
 
-    public String getAlias() {
-        return alias;
-    }
+	public String getAlias() {
+		return alias;
+	}
 
-    public void setAlias(String alias) {
-        this.alias = alias;
-    }
+	public void setAlias(String alias) {
+		this.alias = alias;
+	}
 
-    public String getSalt() {
-        if (salt == null) {
-            salt = generateSalt();
-        }
-        return salt;
-    }
+	public String getSalt() {
+		if (salt == null) {
+			salt = generateSalt();
+		}
+		return salt;
+	}
 
-    public void setSalt(String salt) {
-        this.salt = salt;
-    }
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
 
-    private String generateSalt() {
-        SecureRandom random = new SecureRandom();
-        Hasher hasher = Hashing.sha256().newHasher();
-        hasher.putLong(random.nextLong());
-        return hasher.hash().toString();
-    }
+	private String generateSalt() {
+		SecureRandom random = new SecureRandom();
+		Hasher hasher = Hashing.sha256().newHasher();
+		hasher.putLong(random.nextLong());
+		return hasher.hash().toString();
+	}
 
-    public void resetPasswordHash() {
-        if (password != null && !password.isEmpty()) {
-            setPassword(getPassword());
-        }
-    }
+	public void resetPasswordHash() {
+		if (password != null && !password.isEmpty()) {
+			setPassword(getPassword());
+		}
+	}
 
-    public boolean isInUserGroup() {
-        return !(id == anonymous.getId());
-    }
+	public boolean isInUserGroup() {
+		return !(id == anonymous.getId());
+	}
 
-    public boolean isAnonymous() {
-        return this.getId() == getAnonymousUser().getId();
-    }
+	public boolean isAnonymous() {
+		return this.getId() == getAnonymousUser().getId();
+	}
 
-    public String getSearch() {
-        search = name + " " + alias + " " + email;
-        return search;
-    }
+	public String getSearch() {
+		search = name + " " + alias + " " + email;
+		return search;
+	}
 
-    public void setSearch(String search) {
-        this.search = search;
-    }
+	public void setSearch(String search) {
+		this.search = search;
+	}
 
-    public void initFromDto(UserDto dto) {
-        this.setId(dto.getId());
-        this.setRole(dto.getRole());
-        this.setName(dto.getName());
-        this.setSurname(dto.getSurname());
-        this.setAlias(dto.getAlias());
-        this.setAdresse(dto.getAdresse());
-        this.setSociete(dto.getSociete());
-        this.setTel(dto.getTel());
-        this.setSexe(dto.getSexe());
-        this.setPtsFidelite(dto.getPtsFidelite());
-        this.setDateInscription(new java.util.Date(dto.getDateInscription().getTime()));
-        this.setDateNaissance(new java.util.Date(dto.getDateNaissance().getTime()));
-        this.setNewsLetter(dto.isNewsLetter());
-        this.setEmail(dto.getEmail());
-        this.setPassword(dto.getPassword());
-    }
+	public void initFromDto(UserDto dto) {
+		this.setId(dto.getId());
+		this.setRole(dto.getRole());
+		this.setName(dto.getName());
+		this.setSurname(dto.getSurname());
+		this.setAlias(dto.getAlias());
+		this.setAdresse(dto.getAdresse());
+		this.setSociete(dto.getSociete());
+		this.setTel(dto.getTel());
+		this.setSexe(dto.getSexe());
+		this.setPtsFidelite(dto.getPtsFidelite());
+		if (dto.getDateInscription() != null) {
+			this.setDateInscription(new java.util.Date(dto.getDateInscription().getTime()));
+		}
+		if (dto.getDateNaissance() != null) {
+			this.setDateNaissance(new java.util.Date(dto.getDateNaissance().getTime()));
+		}
+		this.setNewsLetter(dto.isNewsLetter());
+		this.setEmail(dto.getEmail());
+		this.setPassword(dto.getPassword());
+	}
 
-    public UserDto convertToDto() {
-        UserDto dto = new UserDto();
-        dto.setId(this.getId());
-        dto.setRole(this.getRole());
-        dto.setName(this.getName());
-        dto.setSurname(this.getSurname());
-        dto.setAlias(this.getAlias());
-        dto.setAdresse(this.getAdresse());
-        dto.setSociete(this.getSociete());
-        dto.setTel(this.getTel());
-        dto.setSexe(this.getSexe());
-        dto.setPtsFidelite(this.getPtsFidelite());
-        dto.setDateInscription(this.getDateInscription());
-        dto.setDateNaissance(this.getDateNaissance());
-        dto.setNewsLetter(this.isNewsLetter());
-        dto.setEmail(this.getEmail());
-        dto.setPassword(this.getPassword());
-        return dto;
-    }
+	public UserDto convertToDto() {
+		UserDto dto = new UserDto();
+		dto.setId(this.getId());
+		dto.setRole(this.getRole());
+		dto.setName(this.getName());
+		dto.setSurname(this.getSurname());
+		dto.setAlias(this.getAlias());
+		dto.setAdresse(this.getAdresse());
+		dto.setSociete(this.getSociete());
+		dto.setTel(this.getTel());
+		dto.setSexe(this.getSexe());
+		dto.setPtsFidelite(this.getPtsFidelite());
+		dto.setDateInscription(this.getDateInscription());
+		dto.setDateNaissance(this.getDateNaissance());
+		dto.setNewsLetter(this.isNewsLetter());
+		dto.setEmail(this.getEmail());
+		dto.setPassword(this.getPassword());
+		return dto;
+	}
 }

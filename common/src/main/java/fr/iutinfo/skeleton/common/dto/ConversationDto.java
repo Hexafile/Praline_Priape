d269691cc096ec6931841007affe07cc4d36c6e1
Hexafile <thomas.plouchart@etudiant.univ-lille1.fr>
2017-03-23 16:37:29 +0100

    Conversation

---
+++ common/src/main/java/fr/iutinfo/skeleton/common/dto/ConversationDto.java
@@ -0,0 +1,56 @@
+package fr.iutinfo.skeleton.common.dto;
+
+import java.security.Principal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConversationDto implements Principal {
+    final static Logger logger = LoggerFactory.getLogger(UserDto.class);
+	private String name;
+	private int demandeurId;
+	private int conseilleId;
+	private boolean termine;
+	private int note;
+
+	public int getDemandeurId() {
+		return demandeurId;
+	}
+
+	public void setDemandeurId(int demandeurId) {
+		this.demandeurId = demandeurId;
+	}
+
+	public int getConseilleId() {
+		return conseilleId;
+	}
+
+	public void setConseilleId(int conseilleId) {
+		this.conseilleId = conseilleId;
+	}
+
+	public boolean isTermine() {
+		return termine;
+	}
+
+	public void setTermine(boolean termine) {
+		this.termine = termine;
+	}
+
+	public int getNote() {
+		return note;
+	}
+
+	public void setNote(int note) {
+		this.note = note;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}

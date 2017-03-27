58912e5fb4af6d2164fb2d63d69ccdbefe39310a
Vincent Roca <vincentroca9@gmail.com>
2017-03-27 14:58:03 +0200

    modif

---
+++ server/src/main/java/fr/iutinfo/skeleton/servlet/Upload.java
@@ -0,0 +1,17 @@
+package fr.iutinfo.skeleton.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet/Upload")
+public class Upload extends HttpServlet {
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		
+	}
+
+}
